b6acd35576ea7d69b1c7063a82be12bf4a4298c9
Alen Melkonyan <alen_melkonyan@mail.ru>
2019-04-25 23:16:36 +0200

    first commit

---
+++ src/main/java/ru.javabegin.training.springlibrary.entities/Genre.java
@@ -0,0 +1,52 @@
+package ru.javabegin.training.springlibrary.entities;
+
+/**
+ * @author Viktor Tulin
+ * @version 1
+ * @since 24.06.2017
+ */
+public class Genre {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Genre genre = (Genre) o;
+
+        if (id != genre.id) return false;
+        if (name != null ? !name.equals(genre.name) : genre.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
